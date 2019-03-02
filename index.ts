1c1b7bf29fc302a107b6cbcfe631bfed1394220d
Kelvin Fichter <kelvinfichter@gmail.com>
2019-03-02 16:23:58 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { EventWatcher, EventWatcherOptions } from './src/event-watcher'
+export { BaseEthProvider } from './src/eth-provider/base-eth-provider'
+export { BaseEventDB } from './src/event-db/base-event-db'
+export { EventFilterOptions } from './src/models'
